fdbe0ffb53180f8b40e9e17c731e7907b4a4a19e
552020 <stefanolombardo@posteo.de>
2024-01-27 14:56:29 +0100

    refine macros names

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,98 +6,88 @@
  * @brief Header file for the Cub3D game.
  */
 
-# include <string.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <math.h>
-# include "lib/MLX42/include/MLX42/MLX42.h"
-# include "lib/libft/src/libft.h"
-#include <sys/stat.h>
+#include "lib/MLX42/include/MLX42/MLX42.h"
+#include "lib/libft/src/libft.h"
 #include <fcntl.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-
-# define S_W 1900
-# define S_H 1000
-# define TILE_SIZE 30
-# define FOV 60
-# define ROTATION_SPEED 0.045
-# define PLAYER_SPEED 4
-
-
-
+#define WINDOW_WIDTH 1900
+#define WINDOW_HEIGHT 1000
+#define TILE_SIZE 30
+#define FOV 60
+// The previous denomination were PLAYER_SPEED and ROTATION_SPEED.
+// ROTATION_SPEED is also a kind of speed of the player. I'm not sure if
+// translation is clear enough Or if it would be better something else, like
+// walkind speed
+#define PLAYER_ROTATION_SPEED 0.045
+#define PLAYER_TRANSLATION_SPEED 4
 
 /**
  * @brief Structure to save the path to textures.
  */
 typedef struct s_textures {
-    mlx_texture_t *no; /**< Path to the north texture. */
-    mlx_texture_t *so; /**< Path to the south texture. */
-    mlx_texture_t *we; /**< Path to the west texture. */
-    mlx_texture_t *ea; /**< Path to the east texture. */
-    mlx_texture_t *f; /**< Path to the floor texture. */
-    mlx_texture_t *c; /**< Path to the ceiling texture. */
+  mlx_texture_t *no; /**< Path to the north texture. */
+  mlx_texture_t *so; /**< Path to the south texture. */
+  mlx_texture_t *we; /**< Path to the west texture. */
+  mlx_texture_t *ea; /**< Path to the east texture. */
+  mlx_texture_t *f;  /**< Path to the floor texture. */
+  mlx_texture_t *c;  /**< Path to the ceiling texture. */
 } t_textures;
-
-
 
 /**
  * @brief Structure to represent the player in the game.
  */
 typedef struct s_player {
-    int plyr_x; /**< Player x position in pixels. */
-    int plyr_y; /**< Player y position in pixels. */
-    double angle; /**< Player angle. */
-    float fov_rd; /**< Field of view in radians. */
-    int rot; /**< Rotation flag. */
-    int l_r; /**< Left-right flag. */
-    int u_d; /**< Up-down flag. */
+  int plyr_x;   /**< Player x position in pixels. */
+  int plyr_y;   /**< Player y position in pixels. */
+  double angle; /**< Player angle. */
+  float fov_rd; /**< Field of view in radians. */
+  int rot;      /**< Rotation flag. */
+  int l_r;      /**< Left-right flag. */
+  int u_d;      /**< Up-down flag. */
 } t_player;
 
 /**
  * @brief Structure to represent a ray in the game.
  */
 typedef struct s_ray {
-    double ray_ngl; /**< Ray angle. */
-    double distance; /**< Distance to the wall. */
-    int flag; /**< Flag for the wall. */
+  double ray_ngl;  /**< Ray angle. */
+  double distance; /**< Distance to the wall. */
+  int flag;        /**< Flag for the wall. */
 } t_ray;
 
 /**
  * @brief Structure to represent game data, including the map.
  */
 typedef struct s_map {
-    char **map2d; /**< 2D array representing the map. */
-    int p_x; /**< Player x position in the map. */
-    int p_y; /**< Player y position in the map. */
-    int w_map; /**< Map width. */
-    int h_map; /**< Map height. */
+  char **map2d; /**< 2D array representing the map. */
+  int p_x;      /**< Player x position in the map. */
+  int p_y;      /**< Player y position in the map. */
+  int w_map;    /**< Map width. */
+  int h_map;    /**< Map height. */
 } t_map;
 
 /**
  * @brief Structure to hold MLX-related elements and game data.
  */
 typedef struct s_mlx {
-    mlx_image_t *img; /**< Pointer to the image. */
-    mlx_t *mlx_p; /**< Pointer to the MLX instance. */
-    t_ray *ray; /**< Pointer to the ray structure. */
-    t_map *dt; /**< Pointer to the data structure. */
-    t_player *ply; /**< Pointer to the player structure. */
-    t_textures *textures; /**< Pointer to the textures structure. */
+  mlx_image_t *img;     /**< Pointer to the image. */
+  mlx_t *mlx_p;         /**< Pointer to the MLX instance. */
+  t_ray *ray;           /**< Pointer to the ray structure. */
+  t_map *dt;            /**< Pointer to the data structure. */
+  t_player *ply;        /**< Pointer to the player structure. */
+  t_textures *textures; /**< Pointer to the textures structure. */
 } t_mlx;
 
+// PARSER:
+void parse_map(char *argv, t_map *map);
+char *cub_to_str(char *map);
 
-
-
-
-//PARSER:
-void	parse_map(char *argv, t_map *map);
-char	*cub_to_str(char *map);
-
-
-
-
-
-//TEXTURES:
+// TEXTURES:
 /**
  * @brief Get the value after a specified identifier in a string.
  *
@@ -106,16 +96,13 @@ char	*cub_to_str(char *map);
  *
  * @param val The value to search for.
  * @param identifier The identifier to search for in the string.
- * @return A dynamically allocated string containing the value after the identifier.
- *         The caller is responsible for freeing the memory using free().
- *         If the identifier is not found or memory allocation fails, it returns NULL.
+ * @return A dynamically allocated string containing the value after the
+ * identifier. The caller is responsible for freeing the memory using free(). If
+ * the identifier is not found or memory allocation fails, it returns NULL.
  */
 char *get_identifier_value(char *val, char *identifier);
 
-int	load_textures(t_mlx *mlx, char *map_str);
-
-
-
+int load_textures(t_mlx *mlx, char *map_str);
 
 /**
  * @brief Function to handle cleanup and exit the game.
@@ -140,7 +127,8 @@ void mlx_key(mlx_key_data_t keydata, void *ml);
 /**
  * @brief Function to rotate the player based on key input.
  * @param mlx Pointer to the t_mlx structure.
- * @param i Integer indicating the direction of rotation (1 for right, 0 for left).
+ * @param i Integer indicating the direction of rotation (1 for right, 0 for
+ * left).
  */
 void rotate_player(t_mlx *mlx, int i);
 
@@ -188,7 +176,8 @@ void draw_floor_ceiling(t_mlx *mlx, int ray, int t_pix, int b_pix);
 /**
  * @brief Function to get the color of the wall based on its orientation.
  * @param mlx Pointer to the t_mlx structure.
- * @param flag Flag indicating wall orientation (0 for vertical, 1 for horizontal).
+ * @param flag Flag indicating wall orientation (0 for vertical, 1 for
+ * horizontal).
  * @return Color of the wall.
  */
 int get_color(t_mlx *mlx, int flag);
@@ -216,7 +205,8 @@ void render_wall(t_mlx *mlx, int ray);
 void cast_rays(t_mlx *mlx);
 
 /**
- * @brief Function to check if a point lies in a specified quadrant of the unit circle.
+ * @brief Function to check if a point lies in a specified quadrant of the unit
+ * circle.
  * @param angle Angle to check.
  * @param c Character indicating the axis ('x' or 'y').
  * @return 1 if the point lies in the specified quadrant, 0 otherwise.
@@ -224,11 +214,14 @@ void cast_rays(t_mlx *mlx);
 int unit_circle(float angle, char c);
 
 /**
- * @brief Function to check and update intersection points for horizontal or vertical walls.
+ * @brief Function to check and update intersection points for horizontal or
+ * vertical walls.
  * @param angle Angle of the ray.
- * @param inter Pointer to the intersection point, updated based on the direction.
+ * @param inter Pointer to the intersection point, updated based on the
+ * direction.
  * @param step Pointer to the step size, adjusted based on the direction.
- * @param is_horizon Flag indicating whether the intersection is for a horizontal (1) or vertical (0) direction.
+ * @param is_horizon Flag indicating whether the intersection is for a
+ * horizontal (1) or vertical (0) direction.
  * @return -1 if there is an intersection, 1 otherwise.
  */
 int inter_check(float angle, float *inter, float *step, int is_horizon);
@@ -265,7 +258,8 @@ float get_v_inter(t_mlx *mlx, float angl);
 void cast_rays(t_mlx *mlx);
 
 /**
- * @brief Game loop function to handle player movement, ray casting, and rendering.
+ * @brief Game loop function to handle player movement, ray casting, and
+ * rendering.
  * @param ml Pointer to the t_mlx structure.
  */
 void game_loop(void *ml);
