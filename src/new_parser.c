544fe86f394145e6ec0f0786c17b72c1974b89cd
552020 <stefanolombardo@posteo.de>
2024-01-29 23:56:38 +0100

    path and rgb collection done

--- src/new_parser.c
+++ src/new_parser.c
@@ -73,24 +73,144 @@ void	collect_elements_data_path(char *line, char *identifier, char *target)
 	}
 }
 
-void	collect_elements_data_rgba(char line, char *identifier, char *target)
+void	free_str_arr(char **str_arr)
 {
+	int	i;
+
+	i = 0;
+	while (str_arr[i] != NULL)
+	{
+		free(str_arr[i]);
+		i++;
+	}
+	free(str_arr);
 }
 
-void	collect_elements_data(char *line, char *identifier, char *target)
+int	str_isdigit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (FAILURE);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+void	collect_elements_data_rgba(char *line, char *identifier, t_data *data)
+{
+	char	*start;
+	char	*end;
+	char	*rgb_str;
+	char	**rgb_values;
+	int		rgb[3];
+	int		i;
+	t_rgba	*target;
+
+	target = NULL;
+	if (ft_strncmp(identifier, "C", ft_strlen(identifier)) == 0)
+		target = &data->map->c;
+	else if (ft_strncmp(identifier, "F", ft_strlen(identifier)) == 0)
+		target = &data->map->f;
+	// Skip spaces
+	while (*line == ' ')
+		line++;
+	// Skip identifier
+	line += ft_strlen(identifier);
+	// Skip spaces
+	while (*line == ' ')
+		line++;
+	// Extract RGB string
+	start = line;
+	while (*line != ' ' && *line != '\0')
+		line++;
+	end = line;
+	rgb_str = ft_substr(start, 0, end - start);
+	// Split RGB string
+	rgb_values = ft_split(rgb_str, ',');
+	free(rgb_str);
+	// Check length of RGB array
+	i = 0;
+	while (rgb_values[i] != NULL)
+		i++;
+	if (i != 3)
+	{
+		printf("Error\n");
+		printf("Invalid RGB values\n");
+		free_str_arr(rgb_values);
+		// free whatever was allocated before
+		exit(EXIT_FAILURE);
+	}
+	// Check for not int values
+	i = 0;
+	while (rgb_values[i] != NULL)
+	{
+		if (!str_isdigit(rgb_values[i]))
+		{
+			printf("Error\n");
+			printf("Invalid RGB values\n");
+			free_str_arr(rgb_values);
+			// free whatever was allocated before
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	// Convert and validate RGB values
+	i = 0;
+	while (rgb_values[i] != NULL)
+	{
+		rgb[i] = ft_atoi(rgb_values[i]);
+		if (rgb[i] < 0 || rgb[i] > 255)
+		{
+			printf("Error\n");
+			printf("Invalid RGB values\n");
+			free_str_arr(rgb_values);
+			// free whatever was allocated before
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	// Skip spaces
+	line++;
+	while (*line == ' ')
+		line++;
+	// Check for extra data
+	if (*line != '\0')
+	{
+		printf("Error\n");
+		printf("Extra data after %s identifier\n", identifier);
+		free_str_arr(rgb_values);
+		// free whatever was allocated before
+		exit(EXIT_FAILURE);
+	}
+	// Assign RGB values to target
+	target->r = rgb[0];
+	target->g = rgb[1];
+	target->b = rgb[2];
+	free_str_arr(rgb_values);
+}
+
+void	collect_elements_data(char *line, char *identifier, t_data *data)
 {
 	if (ft_strncmp(identifier, "NO", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier, target);
+		collect_elements_data_path(line, identifier,
+			data->textures_paths->north);
 	else if (ft_strncmp(identifier, "SO", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier, target);
+		collect_elements_data_path(line, identifier,
+			data->textures_paths->south);
 	else if (ft_strncmp(identifier, "WE", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier, target);
+		collect_elements_data_path(line, identifier,
+			data->textures_paths->west);
 	else if (ft_strncmp(identifier, "EA", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier, target);
+		collect_elements_data_path(line, identifier,
+			data->textures_paths->east);
 	else if (ft_strncmp(identifier, "C", ft_strlen(identifier)) == 0)
-		collect_elements_data_rgba(line, identifier, target);
+		collect_elements_data_rgba(line, identifier, data);
 	else if (ft_strncmp(identifier, "F", ft_strlen(identifier)) == 0)
-		collect_elements_data_rgba(line, identifier, target);
+		collect_elements_data_rgba(line, identifier, data);
 	else
 	{
 		printf("Error\n");
@@ -127,17 +247,17 @@ void	process_map(char **lines_arr, t_data *data)
 			line++;
 		// Process map elements
 		if (process_map_elements(line, "NO", &elements.no))
-			collect_elements_data(line, "NO", data->textures_paths->north);
+			collect_elements_data(line, "NO", data);
 		else if (process_map_elements(line, "SO", &elements.so))
-			collect_elements_data(line, "SO", data->textures_paths->south);
+			collect_elements_data(line, "SO", data);
 		else if (process_map_elements(line, "WE", &elements.we))
-			collect_elements_data(line, "WE", data->textures_paths->west);
+			collect_elements_data(line, "WE", data);
 		else if (process_map_elements(line, "EA", &elements.ea))
-			collect_elements_data(line, "EA", data->textures_paths->east);
+			collect_elements_data(line, "EA", data);
 		else if (process_map_elements(line, "C", &elements.c))
-			collect_elements_data(line, "C", data->textures_paths->ceiling);
+			collect_elements_data(line, "C", data);
 		else if (process_map_elements(line, "F", &elements.f))
-			collect_elements_data(line, "F", data->textures_paths->floor);
+			collect_elements_data(line, "F", data);
 		else
 		{
 			printf("Error\n");
