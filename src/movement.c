ec5a5aea0c5a4cfb1bdcd015b2210f68dce390b9
Leonard Mangallon <lmangall@c1b3c3.42berlin.de>
2024-02-02 20:11:44 +0100

    artificial movement for debugging

--- src/movement.c
+++ src/movement.c
@@ -27,24 +27,19 @@ void	mlx_key(mlx_key_data_t keydata, void *tmp) // key press
 
 	data = (t_data *)tmp;
 	if (keydata.key == MLX_KEY_ESCAPE && (keydata.action == MLX_PRESS
-			|| keydata.action == MLX_REPEAT)) // exit the game
+			|| keydata.action == MLX_REPEAT))
 		free_exit(data);
 	else if (keydata.key == MLX_KEY_A && (keydata.action == MLX_PRESS))
-		// move left
 		data->player->lateral_move = L_LEFT;
 	else if (keydata.key == MLX_KEY_D && (keydata.action == MLX_PRESS))
-		// move right
 		data->player->lateral_move = L_RIGHT;
 	else if (keydata.key == MLX_KEY_S && (keydata.action == MLX_PRESS))
-		// move down
 		data->player->longitudinal_move = BACKWARD;
-	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS) // move up
+	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
 		data->player->longitudinal_move = FORWARD;
 	else if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
-		// rotate left
 		data->player->rotation = R_LEFT;
 	else if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
-		// rotate right
 		data->player->rotation = R_RIGHT;
 	ft_reles(keydata, data); // release the key
 }
