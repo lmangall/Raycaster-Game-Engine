4ea531e3807a7e397080b1d6c8915cda033dca03
552020 <stefanolombardo@posteo.de>
2024-02-05 00:29:35 +0100

    split process map content

--- src/parser/process_map_content.c
+++ src/parser/process_map_content.c
@@ -71,53 +71,61 @@ int	line_has_not_only_spaces(char *line, int *i)
 	return (FAILURE);
 }
 
+void	check_lines(char **lines_arr, int *i, t_data *data, int first_line,
+		int last_line)
+{
+	remove_new_line_char(lines_arr, *i, data);
+	if (lines_arr[*i][0] == '\0')
+		free_exit_parser(data, "Empty line in map");
+	if (line_has_not_only_spaces(lines_arr[*i], i) == FAILURE)
+		free_exit_parser(data, "Empty (only space) line in map");
+	if (has_only_valid_chars(lines_arr[*i]) == FAILURE)
+		free_exit_parser(data, "Invalid character in map");
+	if (is_surrounded_by_walls(lines_arr, *i, first_line, data->map->height
+			- 1) == FAILURE)
+		free_exit_parser(data, "Map is not surrounded by walls");
+	if (spaces_are_surrounded_by_walls(lines_arr, *i, first_line,
+			last_line) == FAILURE)
+		free_exit_parser(data, "Spaces are not surrounded by walls");
+}
+
+void	build_grid(t_data *data, char **lines_arr, int first_line)
+{
+	int	i;
+
+	i = 0;
+	data->map->grid = ft_calloc(data->map->height + 1, sizeof(char *));
+	if (!data->map->grid)
+		free_exit_parser(data, "Malloc failed");
+	data->map->grid[data->map->height] = NULL;
+	while (i < data->map->height)
+	{
+		data->map->grid[i] = ft_calloc(data->map->width, sizeof(char));
+		if (!data->map->grid[i])
+			free_exit_parser(data, "Malloc failed");
+		ft_strlcpy(data->map->grid[i], lines_arr[first_line + i],
+			data->map->width + 1);
+		i++;
+	}
+}
+
 void	process_map_content(char **lines_arr, t_data *data, int first_line)
 {
 	int	i;
-	int	map_width;
-	int	map_height;
 	int	last_line;
 
 	i = first_line;
-	map_width = ft_strlen(lines_arr[first_line]);
-	map_height = calculate_height(lines_arr, first_line);
-	last_line = first_line + map_height - 1;
+	data->map->width = ft_strlen(lines_arr[first_line]);
+	data->map->height = calculate_height(lines_arr, first_line);
+	last_line = first_line + data->map->height - 1;
 	while (lines_arr[i] != NULL)
 	{
-		remove_new_line_char(lines_arr, i, data);
-		if (lines_arr[i][0] == '\0')
-			free_exit_parser(data, "Empty line in map");
-		if (line_has_not_only_spaces(lines_arr[i], &i) == FAILURE)
-			free_exit_parser(data, "Empty (only space) line in map");
-		if (has_only_valid_chars(lines_arr[i]) == FAILURE)
-			free_exit_parser(data, "Invalid character in map");
-		if (is_surrounded_by_walls(lines_arr, i, first_line, map_height
-				- 1) == FAILURE)
-			free_exit_parser(data, "Map is not surrounded by walls");
-		if (spaces_are_surrounded_by_walls(lines_arr, i, first_line,
-				last_line) == FAILURE)
-			free_exit_parser(data, "Spaces are not surrounded by walls");
+		check_lines(lines_arr, &i, data, first_line, last_line);
 		extract_player_position(lines_arr[i], i - first_line, data);
-		map_width = find_max_width(lines_arr, map_width);
+		data->map->width = find_max_width(lines_arr, data->map->width);
 		i++;
 	}
 	if (data->map->player_found == NOT_FOUND)
 		free_exit_parser(data, "No player found");
-	data->map->width = map_width;
-	data->map->height = map_height;
-	printf("map_height before: %d\n", map_height);
-	data->map->grid = ft_calloc(map_height + 1, sizeof(char *));
-	if (!data->map->grid)
-		free_exit_parser(data, "Malloc failed");
-	data->map->grid[map_height] = NULL;
-	i = 0;
-	while (i < map_height)
-	{
-		data->map->grid[i] = ft_calloc(map_width, sizeof(char));
-		if (!data->map->grid[i])
-			free_exit_parser(data, "Malloc failed");
-		ft_strlcpy(data->map->grid[i], lines_arr[first_line + i], map_width
-			+ 1);
-		i++;
-	}
+	build_grid(data, lines_arr, first_line);
 }
