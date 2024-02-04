416ea52699fc52f9944a2fc41f826aaa4c905407
552020 <stefanolombardo@posteo.de>
2024-02-04 11:46:20 +0100

    eliminate collect_elements_data and use directly collect_elements_data_path and collect_elements_data_rgba in process_map_elements_line

--- src/parser/process_map_elements.c
+++ src/parser/process_map_elements.c
@@ -13,26 +13,6 @@
 #include "cub3d.h"
 #include "parser.h"
 
-int	check_identifier(char *line, const char *identifier,
-		t_status *element_status, t_data *data)
-{
-	if (ft_strncmp(line, identifier, ft_strlen(identifier)) == 0)
-	{
-		if (*element_status == FOUND)
-		{
-			free_exit_parser(data, "Error: Duplicate identifier");
-			return (FAILURE);
-		}
-		else
-		{
-			*element_status = FOUND;
-			return (SUCCESS);
-		}
-	}
-	else
-		return (FAILURE);
-}
-
 void	collect_elements_data_path(char *line, char *identifier, char **target,
 		t_data *data)
 {
@@ -51,61 +31,78 @@ void	collect_elements_data_path(char *line, char *identifier, char **target,
 	*target = ft_substr(start, 0, end - start);
 	while (*line == ' ')
 		line++;
+	if (line != NULL)
+		while (*line == ' ' && *line != '\0')
+			line++;
 	if (*line != '\0')
-		free_exit_parser(data, "Error: Extra data after identifier");
+		free_exit_parser(data, "Extra data after Path identifier");
 }
 
-void	collect_elements_data(char *line, char *identifier, t_data *data)
+// void	collect_elements_data(char *line, char *identifier, t_data *data)
+// {
+// 	if (ft_strncmp(identifier, "NO", ft_strlen(identifier)) == 0)
+// 		collect_elements_data_path(line, identifier,
+// 			&data->map->textures_paths->north, data);
+// 	else if (ft_strncmp(identifier, "SO", ft_strlen(identifier)) == 0)
+// 		collect_elements_data_path(line, identifier,
+// 			&data->map->textures_paths->south, data);
+// 	else if (ft_strncmp(identifier, "WE", ft_strlen(identifier)) == 0)
+// 		collect_elements_data_path(line, identifier,
+// 			&data->map->textures_paths->west, data);
+// 	else if (ft_strncmp(identifier, "EA", ft_strlen(identifier)) == 0)
+// 		collect_elements_data_path(line, identifier,
+// 			&data->map->textures_paths->east, data);
+// 	else if (ft_strncmp(identifier, "C", ft_strlen(identifier)) == 0)
+// 		collect_elements_data_rgba(line, identifier, data);
+// 	else if (ft_strncmp(identifier, "F", ft_strlen(identifier)) == 0)
+// 		collect_elements_data_rgba(line, identifier, data);
+// 	else
+// 		free_exit_parser(data, "Invalid identifier");
+// }
+
+int	check_identifier(char *line, const char *identifier,
+		t_status *element_status, t_data *data)
 {
-	if (ft_strncmp(identifier, "NO", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier,
-			&data->map->textures_paths->north, data);
-	else if (ft_strncmp(identifier, "SO", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier,
-			&data->map->textures_paths->south, data);
-	else if (ft_strncmp(identifier, "WE", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier,
-			&data->map->textures_paths->west, data);
-	else if (ft_strncmp(identifier, "EA", ft_strlen(identifier)) == 0)
-		collect_elements_data_path(line, identifier,
-			&data->map->textures_paths->east, data);
-	else if (ft_strncmp(identifier, "C", ft_strlen(identifier)) == 0)
-		collect_elements_data_rgba(line, identifier, data);
-	else if (ft_strncmp(identifier, "F", ft_strlen(identifier)) == 0)
-		collect_elements_data_rgba(line, identifier, data);
-	else
+	if (ft_strncmp(line, identifier, ft_strlen(identifier)) == 0)
 	{
-		printf("Error\n");
-		printf("Invalid identifier\n");
-		// free whatever was allocated before
-		exit(EXIT_FAILURE);
+		if (*element_status == FOUND)
+		{
+			free_exit_parser(data, "Duplicate identifier");
+			return (FAILURE);
+		}
+		else
+		{
+			*element_status = FOUND;
+			return (SUCCESS);
+		}
 	}
+	else
+		return (FAILURE);
 }
 
 void	process_map_elements_line(char *line, t_data *data,
 		t_map_elements_check *elements)
 {
-	printf("process_map_elements START\n");
 	if (line[0] == '\0' || line[0] == '\n')
-	{
-		printf("Empty line skipped in process_map_elements\n");
 		return ;
-	}
 	while (*line == ' ')
 		line++;
 	if (check_identifier(line, "NO", &elements->no, data) == SUCCESS)
-		collect_elements_data(line, "NO", data);
+		collect_elements_data_path(line, "NO",
+			&data->map->textures_paths->north, data);
 	else if (check_identifier(line, "SO", &elements->so, data) == SUCCESS)
-		collect_elements_data(line, "SO", data);
+		collect_elements_data_path(line, "SO",
+			&data->map->textures_paths->south, data);
 	else if (check_identifier(line, "WE", &elements->we, data) == SUCCESS)
-		collect_elements_data(line, "WE", data);
+		collect_elements_data_path(line, "WE", &data->map->textures_paths->west,
+			data);
 	else if (check_identifier(line, "EA", &elements->ea, data) == SUCCESS)
-		collect_elements_data(line, "EA", data);
+		collect_elements_data_path(line, "EA", &data->map->textures_paths->east,
+			data);
 	else if (check_identifier(line, "C", &elements->c, data) == SUCCESS)
-		collect_elements_data(line, "C", data);
+		collect_elements_data_rgba(line, "C", data);
 	else if (check_identifier(line, "F", &elements->f, data) == SUCCESS)
-		collect_elements_data(line, "F", data);
+		collect_elements_data_rgba(line, "F", data);
 	else
 		error_exit("Invalid identifier");
-	printf("process_map_elements END\n");
 }
