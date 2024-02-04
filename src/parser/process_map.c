3cfac74022957cd72648d14d3f566eefe14fa21e
552020 <stefanolombardo@posteo.de>
2024-02-04 01:13:56 +0100

    change printfs into ft_printfs

--- src/parser/process_map.c
+++ src/parser/process_map.c
@@ -28,17 +28,17 @@ void	free_str_arr(char **str_arr)
 
 int	all_elements_found(t_map_elements_check *elements)
 {
-	printf("Checking if all elements have been found...\n");
+	ft_printf("Checking if all elements have been found...\n");
 	if (elements->no == FOUND && elements->so == FOUND && elements->we == FOUND
 		&& elements->ea == FOUND && elements->c == FOUND
 		&& elements->f == FOUND)
 	{
-		printf("All elements found\n");
+		ft_printf("All elements found\n");
 		return (SUCCESS);
 	}
 	else
 	{
-		printf("Not all elements found\n");
+		ft_printf("Not all elements found\n");
 		return (FAILURE);
 	}
 }
@@ -46,29 +46,29 @@ int	all_elements_found(t_map_elements_check *elements)
 void	print_elements_status(t_map_elements_check *elements)
 {
 	if (elements->no == NOT_FOUND)
-		printf("NO : NOT_FOUND\n");
+		ft_printf("NO : NOT_FOUND\n");
 	else
-		printf("NO : FOUND\n");
+		ft_printf("NO : FOUND\n");
 	if (elements->so == NOT_FOUND)
-		printf("SO : NOT_FOUND\n");
+		ft_printf("SO : NOT_FOUND\n");
 	else
-		printf("SO : FOUND\n");
+		ft_printf("SO : FOUND\n");
 	if (elements->we == NOT_FOUND)
-		printf("WE: NOT FOUND\n");
+		ft_printf("WE: NOT FOUND\n");
 	else
-		printf("WE: FOUND\n");
+		ft_printf("WE: FOUND\n");
 	if (elements->ea == NOT_FOUND)
-		printf("EA: NOT FOUND\n");
+		ft_printf("EA: NOT FOUND\n");
 	else
-		printf("EA: FOUND\n");
+		ft_printf("EA: FOUND\n");
 	if (elements->c == NOT_FOUND)
-		printf("C: NOT FOUND\n");
+		ft_printf("C: NOT FOUND\n");
 	else
-		printf("C: FOUND\n");
+		ft_printf("C: FOUND\n");
 	if (elements->f == NOT_FOUND)
-		printf("F: NOT FOUND\n");
+		ft_printf("F: NOT FOUND\n");
 	else
-		printf("F: FOUND\n");
+		ft_printf("F: FOUND\n");
 }
 
 void	print_str_arr(char **str_arri, int map_height)
@@ -78,31 +78,31 @@ void	print_str_arr(char **str_arri, int map_height)
 	i = 0;
 	while (i < map_height)
 	{
-		printf("%s\n", str_arri[i]);
+		ft_printf("%s\n", str_arri[i]);
 		i++;
 	}
 }
 
 void	print_texture_paths(t_textures_paths *textures_paths)
 {
-	printf("The saved texture paths are:\n");
-	printf("textures_paths->north: %s\n", textures_paths->north);
-	printf("textures_paths->south: %s\n", textures_paths->south);
-	printf("textures_paths->west: %s\n", textures_paths->west);
-	printf("textures_paths->east: %s\n", textures_paths->east);
+	ft_printf("The saved texture paths are:\n");
+	ft_printf("textures_paths->north: %s\n", textures_paths->north);
+	ft_printf("textures_paths->south: %s\n", textures_paths->south);
+	ft_printf("textures_paths->west: %s\n", textures_paths->west);
+	ft_printf("textures_paths->east: %s\n", textures_paths->east);
 }
 
 void	print_ceiling_and_floor(t_rgba *c, t_rgba *f)
 {
-	printf("The saved ceiling and floor colors are:\n");
-	printf("C: %d, %d, %d, %d\n", c->r, c->g, c->b, c->a);
-	printf("F: %d, %d, %d, %d\n", f->r, f->g, f->b, f->a);
+	ft_printf("The saved ceiling and floor colors are:\n");
+	ft_printf("C: %d, %d, %d, %d\n", c->r, c->g, c->b, c->a);
+	ft_printf("F: %d, %d, %d, %d\n", f->r, f->g, f->b, f->a);
 }
 
-void	print_map_elements_check(t_data *data)
+void	print_map_elements(t_data *data)
 {
-	printf("Processing map elements done.\n");
-	printf("The correct number of map elements have been found.\n");
+	ft_printf("Processing map elements done.\n");
+	ft_printf("The correct number of map elements have been found.\n");
 	print_texture_paths(data->map->textures_paths);
 	print_ceiling_and_floor(&data->map->c, &data->map->f);
 }
@@ -111,16 +111,16 @@ void	print_map_final(t_data *data)
 {
 	print_texture_paths(data->map->textures_paths);
 	print_ceiling_and_floor(&data->map->c, &data->map->f);
-	printf("Parsing is complete!\n");
+	ft_printf("Parsing is complete!\n");
 	if (data->map->player_found == FOUND)
-		printf("We have only one player\n");
-	printf("The position of the player is: %d, %d\n", data->map->p_x,
+		ft_printf("We have only one player\n");
+	ft_printf("The position of the player is: %d, %d\n", data->map->p_x,
 		data->map->p_y);
-	printf("The orientation of the player is: %c\n",
+	ft_printf("The orientation of the player is: %c\n",
 		data->map->player_orientation);
-	printf("The map width is: %d\n", data->map->width);
-	printf("The map height is: %d\n", data->map->height);
-	printf("The map grid is:\n");
+	ft_printf("The map width is: %d\n", data->map->width);
+	ft_printf("The map height is: %d\n", data->map->height);
+	ft_printf("The map grid is:\n");
 	print_str_arr(data->map->grid, data->map->height);
 }
 
@@ -149,7 +149,7 @@ void	process_map(char **lines_arr, t_data *data)
 	data->map = map;
 	i = 0;
 	process_map_elements(lines_arr, &i, map, data);
-	print_map_elements_check(data);
+	print_map_elements(data);
 	while (lines_arr[i] != NULL && (lines_arr[i][0] == '\0'
 			|| lines_arr[i][0] == '\n'))
 		i++;
