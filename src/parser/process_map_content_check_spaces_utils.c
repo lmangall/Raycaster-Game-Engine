16afc9b0af2bf366a87c9c0a5bd8ceb28dd0178d
552020 <stefanolombardo@posteo.de>
2024-02-05 15:48:54 +0100

    split functions in files

---
+++ src/parser/process_map_content_check_spaces_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_map_content_check_spaces_utils.c           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slombard <slombard@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 15:48:33 by slombard          #+#    #+#             */
+/*   Updated: 2024/02/05 15:48:37 by slombard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include "parser.h"
+
+int	is_valid_space_sorrounding_char(char c)
+{
+	if (c == ' ' || c == '1')
+		return (SUCCESS);
+	else
+		return (FAILURE);
+}
+
+void	define_start_and_end(int *start, int *end, int idx, int line_len)
+{
+	*start = idx - 1;
+	if (idx - 1 < 0)
+		*start = 0;
+	*end = idx + 1;
+	if (idx + 1 > line_len - 1)
+		*end = line_len - 1;
+}
+
+int	check_same_line(char **lines_arr, int current_line, int idx)
+{
+	int	line_len;
+	int	start;
+	int	end;
+
+	line_len = ft_strlen(lines_arr[current_line]);
+	define_start_and_end(&start, &end, idx, line_len);
+	while (start <= end)
+	{
+		if (!is_valid_space_sorrounding_char(lines_arr[current_line][start]))
+			return (FAILURE);
+		start++;
+	}
+	return (SUCCESS);
+}
+
+int	check_previous_line(char **lines_arr, int current_line, int first_line,
+		int idx)
+{
+	int	line_len;
+	int	start;
+	int	end;
+
+	if (current_line == first_line)
+		return (SUCCESS);
+	line_len = ft_strlen(lines_arr[current_line - 1]);
+	define_start_and_end(&start, &end, idx, line_len);
+	while (start <= end)
+	{
+		if (!is_valid_space_sorrounding_char(lines_arr[current_line
+				- 1][start]))
+			return (FAILURE);
+		start++;
+	}
+	return (SUCCESS);
+}
+
+int	check_next_line(char **lines_arr, int current_line, int last_line, int idx)
+{
+	int	line_len;
+	int	start;
+	int	end;
+
+	if (current_line == last_line)
+		return (SUCCESS);
+	line_len = ft_strlen(lines_arr[current_line + 1]);
+	define_start_and_end(&start, &end, idx, line_len);
+	while (start <= end)
+	{
+		if (!is_valid_space_sorrounding_char(lines_arr[current_line
+				+ 1][start]))
+			return (FAILURE);
+		start++;
+	}
+	return (SUCCESS);
+}
+
+int	spaces_are_surrounded_by_walls(char **lines_arr, int current_line,
+		int first_line, int last_line)
+{
+	int	i;
+
+	i = 0;
+	while (lines_arr[current_line][i] != '\0')
+	{
+		if (lines_arr[current_line][i] == ' ')
+		{
+			if (!check_same_line(lines_arr, current_line, i))
+				return (FAILURE);
+			if (!check_previous_line(lines_arr, current_line, first_line, i))
+				return (FAILURE);
+			if (!check_next_line(lines_arr, current_line, last_line, i))
+				return (FAILURE);
+		}
+		i++;
+	}
+	return (SUCCESS);
+}
