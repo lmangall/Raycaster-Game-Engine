a44fe09eae5da1af6a2218fe55144dc3f8200e26
552020 <stefanolombardo@posteo.de>
2024-02-06 13:50:42 +0100

    refactor check_lines arg numbers

--- src/parser/process_map.c
+++ src/parser/process_map.c
@@ -43,87 +43,6 @@ int	all_elements_found(t_map_elements *elements)
 	}
 }
 
-void	print_elements_status(t_map_elements *elements)
-{
-	if (elements->no == NOT_FOUND)
-		printf("NO : NOT_FOUND\n");
-	else
-		printf("NO : FOUND\n");
-	if (elements->so == NOT_FOUND)
-		printf("SO : NOT_FOUND\n");
-	else
-		printf("SO : FOUND\n");
-	if (elements->we == NOT_FOUND)
-		printf("WE: NOT FOUND\n");
-	else
-		printf("WE: FOUND\n");
-	if (elements->ea == NOT_FOUND)
-		printf("EA: NOT FOUND\n");
-	else
-		printf("EA: FOUND\n");
-	if (elements->c == NOT_FOUND)
-		printf("C: NOT FOUND\n");
-	else
-		printf("C: FOUND\n");
-	if (elements->f == NOT_FOUND)
-		printf("F: NOT FOUND\n");
-	else
-		printf("F: FOUND\n");
-}
-
-void	print_str_arr(char **str_arri, int map_height)
-{
-	int	i;
-
-	i = 0;
-	while (i < map_height)
-	{
-		printf("%s\n", str_arri[i]);
-		i++;
-	}
-}
-
-void	print_texture_paths(t_textures_paths *textures_paths)
-{
-	printf("The saved texture paths are:\n");
-	printf("textures_paths->north: %s\n", textures_paths->north);
-	printf("textures_paths->south: %s\n", textures_paths->south);
-	printf("textures_paths->west: %s\n", textures_paths->west);
-	printf("textures_paths->east: %s\n", textures_paths->east);
-}
-
-void	print_ceiling_and_floor(t_rgba *c, t_rgba *f)
-{
-	printf("The saved ceiling and floor colors are:\n");
-	printf("C: %d, %d, %d\n", c->r, c->g, c->b);
-	printf("F: %d, %d, %d\n", f->r, f->g, f->b);
-}
-
-void	print_map_elements(t_data *data)
-{
-	printf("Processing map elements done.\n");
-	printf("The correct number of map elements have been found.\n");
-	print_texture_paths(data->map->textures_paths);
-	print_ceiling_and_floor(&data->map->c, &data->map->f);
-}
-
-void	final_print(t_data *data)
-{
-	print_texture_paths(data->map->textures_paths);
-	print_ceiling_and_floor(&data->map->c, &data->map->f);
-	printf("Parsing is complete!\n");
-	if (data->map->player_found == FOUND)
-		printf("We have only one player\n");
-	printf("The position of the player is: %d, %d\n", data->map->p_x,
-		data->map->p_y);
-	printf("The orientation of the player is: %c\n",
-		data->map->player_orientation);
-	printf("The map width is: %d\n", data->map->width);
-	printf("The map height is: %d\n", data->map->height);
-	printf("The map grid is:\n");
-	print_str_arr(data->map->grid, data->map->height);
-}
-
 void	process_map(char **lines_arr, t_data *data)
 {
 	t_map_elements	elements;
@@ -149,5 +68,5 @@ void	process_map(char **lines_arr, t_data *data)
 			|| lines_arr[i][0] == '\n'))
 		i++;
 	process_map_content(lines_arr, data, i);
-	final_print(data);
+	print_map_final(data);
 }
