89c8aec06f80428ac2bfbd40e918a65994b33b6b
Leonard Mangallon <lmangall@c1b1c1.42berlin.de>
2024-02-06 20:52:08 +0100

    added buffer distance, now pb occur only on the right side

--- src/caster_to_renderer/movement_utils.c
+++ src/caster_to_renderer/movement_utils.c
@@ -5,21 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmangall <lmangall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/06 11:16:58 by lmangall          #+#    #+#             */
-/*   Updated: 2024/02/06 20:21:05 by lmangall         ###   ########.fr       */
+/*   Created: 2024/02/06 20:49:35 by lmangall          #+#    #+#             */
+/*   Updated: 2024/02/06 20:50:23 by lmangall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+#define BUFFER_DISTANCE (PLAYER_TRANSLATION_SPEED * 1.5)
+
 int	is_wall(t_data *data, double x, double y)
 {
 	int	map_grid_y;
 	int	map_grid_x;
+	int	map_ahead_x;
+	int	map_ahead_y;
 
 	map_grid_x = (x / TILE_SIZE);
 	map_grid_y = (y / TILE_SIZE);
+	map_ahead_x = ((x + BUFFER_DISTANCE) / TILE_SIZE);
+	map_ahead_y = ((y + BUFFER_DISTANCE) / TILE_SIZE);
 	if (data->map->grid[map_grid_y][map_grid_x] == '1'
+		|| data->map->grid[map_ahead_y][map_ahead_x] == '1'
 		|| (data->map->grid[map_grid_y][data->player->x_pos_px
 			/ TILE_SIZE] == '1' || data->map->grid[data->player->y_pos_px
 			/ TILE_SIZE][map_grid_x] == '1'))
