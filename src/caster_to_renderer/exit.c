8808acdb91b9307ae978485cc3ee87c013e4017e
Leonard Mangallon <lmangall@c1b1c1.42berlin.de>
2024-02-06 21:24:54 +0100

    freeing more stuff but still WIP

--- src/caster_to_renderer/exit.c
+++ src/caster_to_renderer/exit.c
@@ -6,42 +6,61 @@
 /*   By: lmangall <lmangall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:15:52 by lmangall          #+#    #+#             */
-/*   Updated: 2024/02/06 12:13:10 by lmangall         ###   ########.fr       */
+/*   Updated: 2024/02/06 21:24:03 by lmangall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// static void free_textures(t_textures *textures)
-// {
-//     if (textures != NULL)
-//     {
-//         // Free individual texture paths
-//         mlx_delete_texture(textures->north);
-//         mlx_delete_texture(textures->south);
-//         mlx_delete_texture(textures->west);
-//         mlx_delete_texture(textures->east);
-//         mlx_delete_texture(textures->floor);
-//         mlx_delete_texture(textures->ceiling);
-//         // Free the t_textures structure
-//         free(textures);
-//     }
-// }
+static void	free_textures(t_textures *textures)
+{
+	if (textures != NULL)
+	{
+		mlx_delete_texture(textures->north);
+		mlx_delete_texture(textures->south);
+		mlx_delete_texture(textures->west);
+		mlx_delete_texture(textures->east);
+		mlx_delete_texture(textures->floor);
+		mlx_delete_texture(textures->ceiling);
+		free(textures);
+	}
+}
+
+static void	free_data(t_data *data)
+{
+	int	i;
+
+	if (data->map != NULL)
+	{
+		if (data->map->grid != NULL)
+		{
+			i = 0;
+			while (data->map->grid[i])
+				free(data->map->grid[i++]);
+			free(data->map->grid);
+		}
+		free(data->map);
+	}
+	if (data->player != NULL)
+	{
+		free(data->player);
+	}
+	if (data->ray != NULL)
+	{
+		free(data->ray);
+	}
+}
 
 void	free_exit(t_data *data)
 {
 	int	i;
 
 	i = 0;
-	while (data->map->grid[i])
-		free(data->map->grid[i++]);
-	free(data->map->grid);
-	free(data->map);
-	free(data->player);
-	free(data->ray);
+	free_textures(data->textures);
 	mlx_delete_image(data->mlx, data->img);
 	mlx_close_window(data->mlx);
 	mlx_terminate(data->mlx);
+	free_data(data);
 	ft_putstr_fd("Tschuess\n", 1);
 	exit(0);
 }
