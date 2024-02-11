cbfdbc3311e1ac0316569a66397f0600e510ebf8
lmangall <l.mangallon@gmail.com>
2024-02-12 00:58:20 +0100

    walls added

--- src/bonus/minimap.c
+++ src/bonus/minimap.c
@@ -29,6 +29,31 @@ static void draw_background(t_data *data)
 	}
 }
 
+static void draw_walls(t_data *data)
+{
+	double scale_x = (double)data->minimap->width / data->map->width;
+	double scale_y = (double)data->minimap->height / data->map->height;
+	int color = RGBA(255, 255, 255, 255); // White color for walls
+
+	int i = 0;
+	while (i < data->map->width)
+	{
+		int j = 0;
+		while (j < data->map->height)
+		{
+			if (data->map->grid[j][i] == '1') // Assuming '1' represents a wall
+			{
+				int x = (int)(i * scale_x);
+				int y = (int)(j * scale_y);
+				data->ray->screen_x = x;
+				render_pixel(data, y, color);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 static void draw_player(t_data *data)
 {
 	int half_size = 3; // Half the size of the square to make it centered on the player
@@ -38,7 +63,6 @@ static void draw_player(t_data *data)
 	int player_pos_y = (int)(data->player->y_pos_px * scale_y);
 	int color = RGBA(255, 0, 0, 255);
 
-	// Draw the four corners of the square using render_pixel
 	render_pixel(data, player_pos_y - half_size, color);
 	render_pixel(data, player_pos_y + half_size, color);
 	render_pixel(data, player_pos_y, color);
@@ -70,8 +94,8 @@ void render_minimap(void *tmp)
 	t_data	*data;
 
 	data = (t_data *)tmp;
-
 	init_minimap(data);
 	draw_background(data);
+	draw_walls(data);
 	draw_player(data);
 }
