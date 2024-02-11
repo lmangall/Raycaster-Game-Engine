0a62ecc35eaa18be910ceabcee59058bfd2c2375
lmangall <l.mangallon@gmail.com>
2024-02-11 15:52:11 +0100

    using only one mlx_image_t for minimap and walls, not deleting the img

--- src/bonus/minimap.c
+++ src/bonus/minimap.c
@@ -4,10 +4,10 @@
 
 void	render_minimap(t_data *data)
 {
-	double scale_x;
-	double scale_y;
-	int player_pos_x;
-	int player_pos_y;
+	// double scale_x;
+	// double scale_y;
+	// int player_pos_x;
+	// int player_pos_y;
 
 
 	int i;
@@ -19,37 +19,34 @@ void	render_minimap(t_data *data)
 	data->minimap->offset_y = 20;
 	i = 0;
 	j = 0;
-	mlx_image_t *image;
-	image = mlx_new_image(data->mlx, data->minimap->width, data->minimap->height);
-	data->minimap->img = image;
 	while (j < data->minimap->height)
 	{
 		i = 0;
 		while (i < data->minimap->width)
 		{
-			mlx_put_pixel(data->minimap->img, i, j, RGBA(0, 0, 0, 255));
+			mlx_put_pixel(data->img, i, j, RGBA(0, 0, 0, 255));
 			i++;
 		}
 		j++;
 	}
-	// Calculate the scale of the minimap
-	scale_x = (double)data->minimap->width / ((double)data->map->width * TILE_SIZE);
-	scale_y = (double)data->minimap->height / ((double)data->map->height * TILE_SIZE);
-	// Calculate the player position on the minimap
-	player_pos_x = (int)data->player->x_pos_px  * scale_x;
-	player_pos_y = (int)data->player->y_pos_px  * scale_y;
+// 	// Calculate the scale of the minimap
+// 	scale_x = (double)data->minimap->width / ((double)data->map->width * TILE_SIZE);
+// 	scale_y = (double)data->minimap->height / ((double)data->map->height * TILE_SIZE);
+// 	// Calculate the player position on the minimap
+// 	player_pos_x = (int)data->player->x_pos_px  * scale_x;
+// 	player_pos_y = (int)data->player->y_pos_px  * scale_y;
 
-	// Draw the player on the minimap as small square
-int half_size = 3; // Half the size of the square to make it centered on the player
-for (int dx = -half_size; dx <= half_size; dx++) {
-    for (int dy = -half_size; dy <= half_size; dy++) {
-        int x = player_pos_x + dx;
-        int y = player_pos_y + dy;
-        if (x >= 0 && x < data->minimap->width && y >= 0 && y < data->minimap->height) {
-            mlx_put_pixel(data->minimap->img, x, y, RGBA(255, 0, 0, 255));
-        }
-    }
-}
+// 	// Draw the player on the minimap as small square
+// int half_size = 3; // Half the size of the square to make it centered on the player
+// for (int dx = -half_size; dx <= half_size; dx++) {
+//     for (int dy = -half_size; dy <= half_size; dy++) {
+//         int x = player_pos_x + dx;
+//         int y = player_pos_y + dy;
+//         if (x >= 0 && x < data->minimap->width && y >= 0 && y < data->minimap->height) {
+//             mlx_put_pixel(data->minimap->img, x, y, RGBA(255, 0, 0, 255));
+//         }
+//     }
+// }
 	
 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmangall <lmangall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:16:31 by lmangall          #+#    #+#             */
-/*   Updated: 2024/02/11 15:27:00 by lmangall         ###   ########.fr       */
+/*   Updated: 2024/02/11 15:48:45 by lmangall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,12 +43,13 @@ void game_hook(void *tmp)
 	t_data	*data;
 
 	data = (t_data *)tmp;
-	if (data->img)
-		mlx_delete_image(data->mlx, data->img);
-	
-	data->img = mlx_new_image(data->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	// if (data->img)
+	// 	mlx_delete_image(data->mlx, data->img);
+		render_background(data->mlx, data->map->c, data->map->f, data);
+
+	// data->img = mlx_new_image(data->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	raycasting(data);
-	mlx_image_to_window(data->mlx, data->img, 0, 0);
+	// mlx_image_to_window(data->mlx, data->img, 0, 0);
 }
 
 void minimap_hook(void *tmp)
@@ -56,12 +57,14 @@ void minimap_hook(void *tmp)
 	t_data	*data;
 
 	data = (t_data *)tmp;
+		// mlx_image_t *image;
+
 	// if (data->minimap->img)
 	// 	mlx_delete_image(data->mlx, data->minimap->img);
-	data->minimap->img = mlx_new_image(data->mlx, data->minimap->width,
-		data->minimap->height);
+	// data->minimap->img = mlx_new_image(data->mlx, data->minimap->width,
+	// 	data->minimap->height);
 	render_minimap(data);
-	mlx_image_to_window(data->mlx, data->minimap->img, 50, 50);
+	// mlx_image_to_window(data->mlx, data->minimap->img, 50, 50);
 }
 
 // place render_background somewhere else ?
@@ -80,9 +83,21 @@ int	main(int argc, char **argv)
 	if (mode == PARSE_ONLY)
 		free_exit_parser(data, "Chill! Parsing only mode is on!");
 	init_data(data);
-	// render_background(data->mlx, data->map->c, data->map->f, data);
 	mlx_loop_hook(data->mlx, &key_hook, data);
+
+
+
+	data->img = mlx_new_image(data->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	mlx_image_to_window(data->mlx, data->img, 0, 0);
+
 	mlx_loop_hook(data->mlx, &game_hook, data);
+
+
+		// mlx_image_t *image;
+	// image = mlx_new_image(data->mlx, data->minimap->width, data->minimap->height);
+	// data->minimap->img = image;
+	// mlx_image_to_window(data->mlx, data->minimap->img, 50, 50);
+
 	mlx_loop_hook(data->mlx, &minimap_hook, data);
 	mlx_loop(data->mlx);
 	free_exit(data);	
