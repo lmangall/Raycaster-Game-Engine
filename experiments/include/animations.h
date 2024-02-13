ac87caaef2c833842d05e7b6db1e8d1a13fe418c
552020 <stefanolombardo@posteo.de>
2024-02-13 20:05:32 +0100

    add experimnts folder and Makefile for Pulga

---
+++ experiments/include/animations.h
@@ -0,0 +1,6 @@
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H
+
+#include <stdio.h>
+
+#endif // ANIMATIONS_H
---
+++ experiments/src/main.c
@@ -0,0 +1,6 @@
+#include "animations.h"
+
+int main() {
+  printf("Hello program!\n");
+  return 0;
+}
