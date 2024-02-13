e39857a3b7f22ff33344630c982698f226e1c367
552020 <stefanolombardo@posteo.de>
2024-02-13 21:07:28 +0100

    simple animation

--- experiments/include/animations.h
+++ experiments/include/animations.h
@@ -1,7 +1,25 @@
 #ifndef ANIMATIONS_H
 #define ANIMATIONS_H
 
+#include "../../MLX42/include/MLX42/MLX42.h"
 #include "../../lib/libft/include/libft.h"
 #include <stdio.h>
+
+typedef struct s_rect {
+  unsigned short int x;
+  unsigned short int y;
+  unsigned short int size_w;
+  unsigned short int size_h;
+  int color;
+} t_rect;
+
+typedef struct s_circle {
+  unsigned short int x;
+  unsigned short int y;
+  unsigned short int radius;
+  int color;
+} t_circle;
+void draw_rect(t_rect rect, mlx_image_t *img);
+void draw_circle(t_circle circle, mlx_image_t *img);
 
 #endif // ANIMATIONS_H
--- experiments/src/main.c
+++ experiments/src/main.c
@@ -1,6 +1,5 @@
 
 
-#include "../../MLX42/include/MLX42/MLX42.h"
 #include "../include/utils.h"
 #include "animations.h"
 #include <signal.h>
@@ -38,6 +37,10 @@ int main(void) {
     /* Put white pixel */
     // put_pixel_img(image, 150, 150, 0x00FFFFFF);
     mlx_put_pixel(image, 150, 150, 0x00FFFFFF);
+    draw_circle((t_circle){100, 120, 23, 0x0002f999}, image);
+    draw_circle((t_circle){150, 250, 15, 0x00f022bf}, image);
+    draw_rect((t_rect){10, 10, 20, 50, 0x005F5599}, image);
+    draw_rect((t_rect){70, 200, 10, 50, 0x0012daf1}, image);
     // mlx_put_image_to_window(image.win.mlx_ptr, image.win.win_ptr,
     // image.img_ptr, 0, 0);
     mlx_image_to_window(tutorial, image, 0, 0);
