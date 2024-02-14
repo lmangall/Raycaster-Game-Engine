856a49ddb59dbc5cd05e91dfae1eb94572f4bd41
552020 <stefanolombardo@posteo.de>
2024-02-14 13:29:10 +0100

    some steps forward

--- src/bonus/minimap.c
+++ src/bonus/minimap.c
@@ -2,6 +2,13 @@
 
 #define RGBA(r, g, b, a) ((r << 24) | (g << 16) | (b << 8) | a)
 
+static int	ft_max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
 static void	init_minimap(t_data *data)
 {
 	data->minimap->height = 150;
@@ -41,30 +48,52 @@ static void	render_background(t_data *data)
 	}
 }
 
+// Why we need ft_max: see Note 1
+
 static void	render_block(int offset_x, int offset_y, t_data *data)
 {
-	int	x;
-	int	y;
-	int	block_width;
-	int	block_height;
-	int	block_color;
+	int		x;
+	int		y;
+	int		block_width_px;
+	int		block_height_px;
+	double	block_width_px_f;
+	double	block_height_px_f;
+	int		block_color;
+	int		offset_x_px;
+	int		offset_y_px;
 
+	offset_x_px = offset_x * TILE_SIZE;
+	offset_y_px = offset_y * TILE_SIZE;
 	// printf("render block\n");
 	y = 0;
 	// Add +1 to include the last pixel and compensate for rounding down
 	// when converting scales to integers.
-	block_width = (int)data->minimap->scale_x + 1;
-	block_height = (int)data->minimap->scale_y + 1;
+	// block_width = (int)data->minimap->scale_x + 1;
+	block_width_px_f = data->minimap->scale_x * TILE_SIZE;
+	block_height_px_f = data->minimap->scale_y * TILE_SIZE;
+	block_width_px = ft_max(block_width_px_f, 1);
+	block_height_px = ft_max(block_height_px_f, 1);
 	block_color = RGBA(255, 255, 255, 255);
-	while (y < block_height)
+	// printf("scale_x: %f, scale_y: %f\n", data->minimap->scale_x,
+	// data->minimap->scale_y);
+	// printf("block_width: %d\n", block_width_px);
+	// printf("block_height: %d\n", block_height_px);
+	while (y < block_height_px)
 	{
 		x = 0;
-		while (x < block_width)
+		while (x < block_width_px)
 		{
-			if (x + offset_x > data->minimap->width || y
-				+ offset_y > data->minimap->height)
+			if (x + offset_x_px >= data->minimap->width || y
+				+ offset_y_px >= data->minimap->height)
+			{
+				printf("x + offset_x_px: %d, y + offset_y_px: %d\n", x
+					+ offset_x_px, y + offset_y_px);
+				printf("break\n");
 				break ;
-			if (render_pixel(data->img, x + offset_x, y + offset_y,
+			}
+			// if (render_pixel(data->img, x + offset_x, y + offset_y,
+			// block_color) == NULL)
+			if (render_pixel(data->img, x + offset_x_px, y + offset_y_px,
 					block_color) == NULL)
 				free_exit(data);
 			x++;
@@ -72,7 +101,9 @@ static void	render_block(int offset_x, int offset_y, t_data *data)
 		y++;
 	}
 }
-
+// offset_x and offset_y are values in tiles, not pixels
+// but they should be converted to pixels to render the block
+// map->width and map->height are in tiles
 static void	render_walls(t_data *data)
 {
 	int	offset_x;
@@ -128,7 +159,7 @@ static void	render_player(t_data *data)
 		x = player_pos_x - half_size;
 		while (x <= player_pos_x + half_size)
 		{
-			if (!(x < 0 || x >= data->minimap->width || y < 0))
+			if ((x < 0 || x >= data->minimap->width || y < 0))
 				break ;
 			if (render_pixel(data->img, x, y, player_color) == NULL)
 				free_exit(data);
@@ -150,7 +181,7 @@ static void	render_ray(t_data *data)
 	int		x;
 	int		y;
 
-	printf("render ray\n");
+	// printf("render ray\n");
 	ray_length = 50;
 	ray_color = RGBA(0, 255, 0, 255);
 	// Calculate the player's position on the minimap
@@ -177,7 +208,7 @@ static void	render_ray(t_data *data)
 
 void	render_minimap(void *tmp)
 {
-	t_data *data;
+	t_data	*data;
 
 	data = (t_data *)tmp;
 	init_minimap(data);
@@ -186,3 +217,12 @@ void	render_minimap(void *tmp)
 	render_player(data);
 	render_ray(data);
 }
+
+// Note 1
+/*
+
+The reason for using ft_max in calculating block_width and block_height is to ensure that every element we attempt to draw on the minimap has a visible representation. When scaling down from the game world to the minimap,
+	the calculated dimensions for smaller elements might round down to zero due to the scale factor. Using ft_max guarantees that each element occupies at least one pixel on the minimap,
+	making sure it's visibly represented even if it's significantly smaller than the scale might suggest.
+
+*/
